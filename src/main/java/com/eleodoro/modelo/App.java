abb2afd02e2497bd9129f9aeb7ba6029817aa837
pimentelcf <vitor.pimentel.santos@escola.pr.gov.br>
2024-06-14 08:39:24 -0300

    terceiro commite

---
+++ src/main/java/com/eleodoro/modelo/App.java
@@ -0,0 +1,15 @@
+package com.eleodoro.modelo;
+
+public class App {
+    
+public static void main (String [] args )
+    {
+
+        System.out.println("s");
+        System.out.println("d");
+        System.out.println("a");
+
+    }
+   
+}
+
--- src/test/java/com/eleodoro/AppTest.java
+++ src/test/java/com/eleodoro/AppTest.java
@@ -17,5 +17,6 @@ public class AppTest
     {
         assertTrue( true );
         System.out.println("pimentel lindo");
+        System.out.println("testando o terceiro comite");
     }
 }
